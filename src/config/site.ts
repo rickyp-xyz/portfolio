58d433c18c3d8c28561945348a40065caeb53cf6
Richard Szilagyi <riszilagyi@proton.me>
2026-08-16 05:53:02 +0700

    add global site config file with personal metadata

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+export const site = {
+    name: "Richard Szilagyi",
+    description: "Software developer...",
+    location: "Thailand",
+    url: "https://rickyp.xyz",
+    contacts: {
+        email: "riszilagyi0@gmail.com",
+        telegram: "",
+    },
+    socials: {
+        github: "",
+        twitter: "",
+        youtube: ""
+    }
+}
